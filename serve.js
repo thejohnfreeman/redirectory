3693e20d27118f1b82cfb6c9685c3ed2c3e5b916
John Freeman <jfreeman08@gmail.com>
2023-07-28 16:08:13 -0500

    Add Google Cloud

--- serve.js
+++ serve.js
@@ -1,7 +1,7 @@
 import express from 'express'
 import router from './router.js'
 
-const port = 9494
+const port = process.env.PORT || 9494
 const app = express()
 app.use(router)
 app.listen(port, () => {
