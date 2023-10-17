aa59265b8a52747c9642f000bc8ebacaeaca69e4
John Freeman <jfreeman08@gmail.com>
2023-10-17 09:36:32 -0500

    Push changes to container

--- tests/container/install.sh
+++ tests/container/install.sh
@@ -10,6 +10,7 @@ gcc_version=${GCC_VERSION:-11}
 cmake_version=${CMAKE_VERSION:-3.25.1}
 cmake_sha256=1c511d09516af493694ed9baf13c55947a36389674d657a2d5e0ccedc6b291d8
 conan_version=${CONAN_VERSION:-1.60}
+node_version=${NODE_VERSION:-20}
 
 # Do not add a stanza to this script without explaining why it is here.
 
@@ -20,6 +21,8 @@ DEBIAN_FRONTEND=noninteractive apt install --yes --no-install-recommends tzdata
 # Iteratively build the list of packages to install so that we can interleave
 # the lines with comments explaining their inclusion.
 dependencies=''
+# - to download Node
+dependencies+=' ca-certificates gnupg'
 # - to download CMake
 dependencies+=' curl'
 # - to build CMake
@@ -36,15 +39,16 @@ dependencies+=' jq tcpdump lsof psmisc'
 apt install --yes ${dependencies}
 
 # Install Node and NPM.
-curl -sL https://deb.nodesource.com/setup_20.x | bash
-apt install --yes nodejs
+curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | gpg --dearmor -o /usr/share/keyrings/nodesource.gpg
+echo "deb [signed-by=/usr/share/keyrings/nodesource.gpg] https://deb.nodesource.com/node_${node_version}.x nodistro main" | tee /etc/apt/sources.list.d/nodesource.list
 
 # Install the GitHub CLI
 curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg
 chmod go+r /usr/share/keyrings/githubcli-archive-keyring.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | tee /etc/apt/sources.list.d/github-cli.list > /dev/null
+
 apt update
-apt install gh --yes
+apt install --yes gh nodejs
 
 # Give us nice unversioned aliases for gcc and company.
 update-alternatives --install \
@@ -96,6 +100,7 @@ conan profile update env.CC=/usr/bin/gcc default
 conan profile update env.CXX=/usr/bin/g++ default
 
 conan remote add redirectory http://localhost
+conan remote add gcloud https://conan.jfreeman.dev
 
 conan install zlib/1.2.13@ --build
 
--- tests/docker.sh
+++ tests/docker.sh
@@ -6,15 +6,14 @@ set -o pipefail
 
 revisions=${REVISIONS:-True}
 remote=${REMOTE:-redirectory}
+image=${IMAGE:-6f5a5f5cb0d7}
 
-image=b3607cc3d8ad
+test=${1:-./tests/test.sh}
 
-sudo docker run --rm --interactive --init \
+sudo docker run --rm --interactive \
 --mount type=bind,source=$(pwd),target=/root/redirectory \
 ${image} <<EOF
-apt install lsof
 cd redirectory
-conan remote add gcloud https://conan.jfreeman.dev --insert 1
 PORT=80 VERBOSE=3 REVISIONS=${revisions} REMOTE=${remote} \
-  ./tests/serve.sh ./tests/test.sh
+  ./tests/serve.sh ${test}
 EOF
