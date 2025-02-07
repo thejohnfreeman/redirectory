0df8bbceceae4d7a555e4e7d190a3a644feee0e6
John Freeman <jfreeman08@gmail.com>
2025-02-06 20:11:25 -0600

    Trim configuration

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,3 @@
 export default {
   preset: 'ts-jest/presets/default-esm',
-  extensionsToTreatAsEsm: ['.ts'],
-  moduleFileExtensions: ['js', 'ts'],
-  transform: {
-    '^.+\\.(ts|tsx)$': ['ts-jest', { useESM: true }],
-  },
-  transformIgnorePatterns: [
-    'node_modules/(?!(octokit)/)',
-  ],
 }
